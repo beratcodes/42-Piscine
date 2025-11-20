991849abf4660ec06ae4ef91294f095dc4c285ef
beratcodes <159455724+beratcodes@users.noreply.github.com>
2025-11-20 21:32:39 +0300

    level 0 is completed.

--- Exams/level0/aff_first_param.c
+++ Exams/level0/aff_first_param.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-    if(argc > 1)
-        while (*argv[1])
-            write(1, argv[1]++, 1);
-    write(1, "\n", 1);    
-    return 0;
+    if (ac > 1)
+        while (*av[1])
+            write(1, av[1]++, 1);
+    write(1, "\n", 1);
+    
+    return (0);
 }
